8114fa205152dcbbd83a263fdc892b648366032e
Cesar Morales <cmoralesmani@gmail.com>
2020-11-20 09:46:37 -0600

    Paquetes y modulos

---
+++ modulos y paquetes/paquetes/paquete1/calculadora_modulo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#_*_ coding: utf8 _*_
+
+def suma(valor1, valor2):
+    print("La suma es: {}".format(valor1+valor2))
+
+def resta(valor1, valor2):
+    print("La resta es: {}".format(valor1-valor2))
+
+def multiplicacion(valor1, valor2):
+    print("La multiplicaión es: {}".format(valor1*valor2))
+
+def division(valor1, valor2):
+    print("La división es: {}".format(valor1/valor2))
